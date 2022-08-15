e7cc055e95fb5c6230eb189585f6924adae5c4ef
tahsinAmin <muhammadtahsinamin@gmail.com>
2022-08-15 22:01:06 +0600

    Added all dynamic where possible

--- components/TableTry/TableTry.js
+++ components/TableTry/TableTry.js
@@ -1,52 +1,109 @@
-import React, { Component } from 'react';
 import { Box, Center, Flex, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr, VStack } from "@chakra-ui/react";
-import { CalendarIcon } from "@chakra-ui/icons";
+import { CheckIcon, Icon } from "@chakra-ui/icons";
+import { GrStackOverflow } from "react-icons/gr";
+import { BsFillMicFill } from "react-icons/bs";
+import { GiHeadphones } from "react-icons/gi";
+import { TiHeadphones } from "react-icons/ti";
+import { MdColorLens, MdTouchApp } from "react-icons/md";
+import { FaBluetooth, FaHandHoldingHeart, FaHeadphones } from "react-icons/fa";
+import { useState } from "react";
+
 
 const TableTry = () => {
 
+    const CircleIcon = (props) => (
+        <Icon viewBox='0 0 200 200' {...props}>
+            <path
+                fill='currentColor'
+                d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
+            />
+        </Icon>
+    )
+
+    const [data, setData] = useState([
+        { "name": "H2", "wWB": false, "tI": true, "rM": true, "m": "Leather, fabric, polymer, rubber", "colors": ["gray.300", "red.300", "blue.300", "pink.300", "gray.300"], "fit": "On-ear" },
+        { "name": "H3 ANIC", "wWB": false, "tI": false, "rM": true, "m": "Stainless steel, polymer, rubber", "colors": ["gray.300", "red.300", "blue.300", "pink.300", "gray.300"], "fit": "Over-ear" },
+        { "name": "H2", "wWB": true, "tI": true, "rM": true, "m": "Aluminimum, polymer, rubber", "colors": ["gray.300", "red.300", "blue.300", "pink.300", "gray.300"], "fit": "On-ear" },
+    ]);
     return (
-        <TableContainer bg='gray.600' color='#fff' h={'30vh'}>
-            <Table size='sm' variant='unstyled'>
+        <TableContainer>
+            <Table size='md' variant='unstyled'>
                 <Thead>
                     <Tr>
                         <Th border={'none'}></Th>
                         <Th border={'none'}></Th>
-                        <Th border={'none'}><Center><VStack><CalendarIcon boxSize={8} />
+                        <Th border={'none'}><Center><VStack><TiHeadphones size={"36px"} />
                             <Text>H2</Text></VStack></Center></Th>
-                        <Th border={'none'}><Center><VStack><CalendarIcon boxSize={8} />
+                        <Th border={'none'}><Center><VStack><GiHeadphones size={"36px"} />
                             <Text>H3 Anic</Text></VStack></Center></Th>
-                        <Th border={'none'}><Center><VStack><CalendarIcon boxSize={8} />
+                        <Th border={'none'}><Center><VStack>
+                            <FaHeadphones size={"36px"} />
                             <Text>H4 Ultra</Text></VStack></Center></Th>
                     </Tr>
                 </Thead>
                 <Tbody>
                     <Tr>
-                        <Td border={'none'} as={Flex} justifyContent={'flex-end'}><CalendarIcon /></Td>
-                        <Td border='1px solid #fff' borderTop={0} borderLeft={0}>Wireless with bluetooth</Td>
-                        <Td border='1px solid #fff' borderTop={0} borderRight={0}></Td>
-                        <Td border='1px solid #fff' borderTop={0} borderRight={0}></Td>
-                        <Td borderRadius='10px' border='1px solid #fff' borderTop={0} borderRight={0}
-                        ><Center><CheckIcon /></Center></Td>
+                        <Td border={'none'} as={Flex} justifyContent={'flex-end'}><FaBluetooth size={"16px"} /></Td>
+                        <Td border='1px solid #fff' borderTop={0} borderLeft={0} borderRight={0}>
+                            Wireless with bluetooth</Td>
+                        {data.map((item) => <Td border='1px solid #fff' borderTop={0} borderRight={0}><Center>{item.wWB && <CheckIcon />}</Center></Td>)}
                     </Tr>
                     <Tr>
-                        <Td border={'none'} as={Flex} justifyContent={'flex-end'}><CalendarIcon /></Td>
-                        <Td border='1px solid #fff' borderTop={0} borderLeft={0} borderRight={0}>feet</Td>
-                        <Td border='1px solid #fff' borderTop={0} borderRight={0}><Center><CheckIcon /></Center></Td>
-                        <Td border='1px solid #fff' borderTop={0} borderRight={0}></Td>
-                        <Td borderRadius='10px' border='1px solid #fff' borderTop={0} borderRight={0}
-                        ><Center><CheckIcon /></Center></Td>
+                        <Td border={'none'} as={Flex} justifyContent={'flex-end'}><MdTouchApp /></Td>
+                        <Td border='1px solid #fff' borderTop={0} borderLeft={0} borderRight={0}>Touch interface</Td>
+                        {data.map((item) => <Td border='1px solid #fff' borderTop={0} borderRight={0}><Center>{item.tI && <CheckIcon />}</Center></Td>)}
+                    </Tr>
+
+                    <Tr>
+                        <Td border={'none'} as={Flex} justifyContent={'flex-end'}><BsFillMicFill /></Td>
+                        <Td border='1px solid #fff' borderTop={0} borderLeft={0} borderRight={0}>Remote + Mic</Td>
+                        {data.map((item) => <Td border='1px solid #fff' borderTop={0} borderRight={0}><Center>{item.rM && <CheckIcon />}</Center></Td>)}
+                    </Tr>
+
+                    <Tr>
+                        <Td border={'none'}><Box as={Flex} justifyContent={'flex-end'} alignItems='center'><GrStackOverflow /></Box></Td>
+                        <Td border='1px solid #fff' borderLeft={0} borderRight={0}
+                            borderTop={0}>Materials</Td>
+                        {data.map((item) => <Td border='1px solid #fff' borderRight={0} borderTop={0}><Center>{item.m}</Center></Td>)}
                     </Tr>
                     <Tr>
-                        <Td border={'none'} as={Flex} justifyContent={'flex-end'}><CalendarIcon /></Td>
-                        <Td border='1px solid #fff' borderTop={0} borderLeft={0} borderRight={0}
-                            borderBottom={0}>yards</Td>
-                        <Td border='1px solid #fff' borderTop={0} borderRight={0} borderBottom={0}><Center><CheckIcon /></Center></Td>
-                        <Td borderRadius='10px' border='1px solid #fff' borderTop={0} borderRight={0}
-                            borderBottom={0}
-                        ><Center><CheckIcon /></Center></Td>
-                        <Td borderRadius='10px' border='1px solid #fff' borderTop={0} borderRight={0}
-                            borderBottom={0}
-                        ><Center><CheckIcon /></Center></Td>
+                        <Td border={'none'} as={Flex} justifyContent={'flex-end'}><MdColorLens /></Td>
+                        <Td border='1px solid #fff' borderTop={0} borderLeft={0} borderRight={0}>Colours</Td>
+                        <Td border='1px solid #fff' borderTop={0} borderRight={0}>
+                            <Flex justifyContent={'center'} gap={1}>
+                                <CircleIcon boxSize={5} color={'gray.300'} />
+                                <CircleIcon boxSize={5} color={'red.300'} />
+                                <CircleIcon boxSize={5} color={'blue.300'} />
+                                <CircleIcon boxSize={5} color={'pink.300'} />
+                                <CircleIcon boxSize={5} color={'gray.600'} />
+                            </Flex>
+                        </Td>
+                        <Td border='1px solid #fff' borderTop={0} borderRight={0}>
+                            <Flex justifyContent={'center'} gap={1}>
+                                <CircleIcon boxSize={5} color={'gray.300'} />
+                                <CircleIcon boxSize={5} color={'red.300'} />
+                                <CircleIcon boxSize={5} color={'blue.300'} />
+                                <CircleIcon boxSize={5} color={'pink.300'} />
+                                <CircleIcon boxSize={5} color={'gray.600'} />
+                            </Flex>
+                        </Td>
+                        <Td border='1px solid #fff' borderTop={0} borderRight={0}>
+                            <Flex justifyContent={'center'} gap={1}>
+                                {/* {data.map((item) => {
+                                    item.map(color => <Box rounded={'full'} h={'12px'} w={'12px'} bg={color} />)
+                                })} */}
+                                <CircleIcon boxSize={5} color={'gray.300'} />
+                                <CircleIcon boxSize={5} color={'red.300'} />
+                                <CircleIcon boxSize={5} color={'blue.300'} />
+                                <CircleIcon boxSize={5} color={'pink.300'} />
+                                <CircleIcon boxSize={5} color={'gray.600'} />
+                            </Flex>
+                        </Td>
+                    </Tr>
+                    <Tr>
+                        <Td border={'none'} as={Flex} justifyContent={'flex-end'}><FaHandHoldingHeart /></Td>
+                        <Td border='1px solid #fff' borderTop={0} borderLeft={0} borderRight={0} borderBottom={0}>Fit</Td>
+                        {data.map((item) => <Td border='1px solid #fff' borderTop={0} borderRight={0} borderBottom={0}><Center>{item.fit}</Center></Td>)}
                     </Tr>
                 </Tbody>
             </Table>
@@ -56,3 +113,9 @@ const TableTry = () => {
 }
 
 export default TableTry;
+
+const data = [
+    { "name": "H2", "wWB": false, "tI": true, "rM": true, "m": "Leather, fabric, polymer, rubber", "colors": ["gray.300", "red.300", "blue.300", "pink.300", "gray.300"], "fit": "On-ear" },
+    { "name": "H3 ANIC", "wWB": false, "tI": false, "rM": true, "m": "Stainless steel, polymer, rubber", "colors": ["gray.300", "red.300", "blue.300", "pink.300", "gray.300"], "fit": "Over-ear" },
+    { "name": "H2", "wWB": true, "tI": true, "rM": true, "m": "Aluminimum, polymer, rubber", "colors": ["gray.300", "red.300", "blue.300", "pink.300", "gray.300"], "fit": "On-ear" },
+]
